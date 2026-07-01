6bf6b318afdeba4d5d5545e9eff335ea6748be24
javiersantaolallapolanco <javier.santaolalla.polanco@gmail.com>
2026-07-01 17:25:38 +0200

    feat: add platform and application indexes

--- scripts/lib/status.sh
+++ scripts/lib/status.sh
@@ -1,16 +1,38 @@
 #!/bin/sh
+
 . "$DOCKSIDE_HOME/scripts/lib/state.sh"
+. "$DOCKSIDE_HOME/scripts/lib/config.sh"
 
 status_show() {
-  state_load
 
-  cat <<EOT
-Dockside status
+    state_load
+    config_load
 
-Platform:     $PLATFORM_STATUS
-Current app:  ${CURRENT_APP:-}
-App status:   $CURRENT_APP_STATUS
-Last start:   $LAST_START
-Last stop:    $LAST_STOP
-EOT
+    echo
+    echo "Dockside"
+    echo "========="
+    echo
+
+    printf "Platform      : %s\n" "$PLATFORM_STATUS"
+    printf "Current App   : %s\n" "${CURRENT_APP:-<none>}"
+    printf "App Status    : %s\n" "$CURRENT_APP_STATUS"
+    printf "Last Start    : %s\n" "$LAST_START"
+    printf "Last Stop     : %s\n" "$LAST_STOP"
+
+    echo
+    echo "Platform stacks"
+    echo "---------------"
+
+    if [ -f "$DOCKER_ROOT/platform/stacks/index" ]; then
+        cat "$DOCKER_ROOT/platform/stacks/index"
+    fi
+
+    echo
+    echo "Installed applications"
+    echo "----------------------"
+
+    if [ -f "$DOCKER_ROOT/apps/index" ]; then
+        cat "$DOCKER_ROOT/apps/index"
+    fi
+
 }
